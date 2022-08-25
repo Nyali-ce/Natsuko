2b6a9d3fdd84ca9597f33a4df0704369569f1450
Nathan Beaumier <2254558@etu.cegep-lanaudiere.qc.ca>
2022-08-25 16:19:50 -0400

    fixed guildDelete event

--- src/commands/tools/say.js
+++ src/commands/tools/say.js
@@ -4,6 +4,7 @@ export default {
     run: async (message, args) => {
         if (!args[0]) return message.channel.send('Please provide a message to send.');
         message.delete();
-        // message.channel.send(args.join(' '));
+        message.channel.send(args.join(' '));
+        console.log(`${message.author.tag} used the say command in ${message.guild.name}: ${args.join(' ')}`);
     }
 }
--- src/events/client/guildDelete.js
+++ src/events/client/guildDelete.js
@@ -4,6 +4,13 @@ export default {
     name: 'guildDelete',
     run: guild => {
         if (!fs.existsSync(`src/data/guilds/${guild.id}`)) return;
+        
+        const guildFolder = fs.readdirSync(`src/data/guilds/${guild.id}`);
+        
+        guildFolder.forEach(file => {
+            fs.unlinkSync(`src/data/guilds/${guild.id}/${file}`);
+        })
+
         fs.rmdirSync(`src/data/guilds/${guild.id}`);
     }
 }
